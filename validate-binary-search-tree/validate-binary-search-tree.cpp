4c555c1468cd2a2fa43ccacd431585be147a7edf
Sadra Manouchehrifar <71413305+sadramanouch@users.noreply.github.com>
2023-08-17 01:16:09 -0700

    Time: 15 ms, Memory: 22.8 MB - LeetHub

---
+++ validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     bool isValidBST(TreeNode* root) {
+//         void inorder(TreeNode* root, vector <int> &nodes){
+//             if(root == nullptr){
+//                 return;
+//             }
+//             inorder(root -> left, nodes);
+//             nodes.push_back(root -> val);    
+//             inorder(root -> right, nodes); 
+//         }
+
+//         bool isValidBST(TreeNode* root) {
+//             if(root == nullptr){
+//                 return true;
+//             }
+//             vector <int> nodes;
+//             inorder(root, nodes); 
+//             set <int> s(nodes.begin(), nodes.end());
+//             if(s.size() != nodes.size()){
+//                 return false;
+//             }
+//             for(int i = 0; i < nodes.size()-1; i++){
+//                 if(nodes[i] > nodes[i+1]){
+//                     return false;
+//                 }
+//             }       
+//             return true;
+//         }
+//     }
+// };
+
+class Solution {
+public:
+
+    void inorder(TreeNode* root, vector <int> &nodes)
+    {
+        if(!root)
+            return;
+
+        inorder(root -> left, nodes);
+        nodes.push_back(root -> val);    
+        inorder(root -> right, nodes); 
+    }
+
+    bool isValidBST(TreeNode* root) 
+    {
+        if(!root)
+            return true;
+
+        vector <int> nodes;
+        inorder(root, nodes); 
+
+        set <int> s(nodes.begin(), nodes.end());
+
+        if(s.size() != nodes.size())
+            return false;
+
+        for(int i = 0; i < nodes.size()-1; i++)
+        {
+            if(nodes[i] > nodes[i+1])
+                return false;
+        }       
+
+        return true;
+    }
+};
