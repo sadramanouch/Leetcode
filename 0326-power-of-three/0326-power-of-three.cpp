ee736b8c97d0bd505e9691622fa5d32f6d179f08
Sadra Manouchehrifar <71413305+sadramanouch@users.noreply.github.com>
2023-08-25 11:57:46 -0700

    Time: 12 ms (80.37%), Space: 6 MB (46.66%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -4,7 +4,7 @@ public:
         if (n == 1){
             return true;
         } 
-        if (n<0 || n == 0 || n%3>0){
+        if (n == 0 || n%3>0){
             return false;
         }
         return isPowerOfThree(n/3);
