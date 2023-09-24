4e2d73306028fa0f2f13786bfb7c95df0a0d4e87
Sadra Manouchehrifar <71413305+sadramanouch@users.noreply.github.com>
2023-09-24 00:13:56 -0700

    Time: 185 ms (65.09%), Space: 131.4 MB (30.31%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        // double sum = 0;
-        // double val = k;
-        // int n = nums.size();
-        // for (int i = 0; i<n; i++){
-        //     for (int j = i; j<n; j++){
-        //         sum += nums[i];
-        //         double g = sum/val;
-        //         if (g == static_cast<int>(g)){
-        //             return true;
-        //         }
-        //     }
-        //     sum = 0;
-        // }
-        // return false;
         int n = nums.size();
-        if (n < 2) return false;
-
+        if (n < 2){
+            return false;
+        } 
         unordered_map<int, int> remainderMap;
-        remainderMap[0] = -1;  // Handle subarrays that start from index 0
-
+        remainderMap[0] = -1; 
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += nums[i];
             int remainder = (k == 0) ? sum : sum % k;
-
             if (remainderMap.find(remainder) != remainderMap.end()) {
                 int prevIndex = remainderMap[remainder];
-                if (i - prevIndex > 1)  // Check subarray length
+                if (i - prevIndex > 1)  
                     return true;
             } else {
                 remainderMap[remainder] = i;
             }
         }
-
         return false;
     }
 };
