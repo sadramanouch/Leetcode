59e91ccd4565373a7c6446480dd2ead228f993d7
Sadra Manouchehrifar <71413305+sadramanouch@users.noreply.github.com>
2023-10-03 12:04:22 -0700

    Time: 4 ms (45.08%), Space: 8.3 MB (45.09%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> occur;
-        unordered_set<int> counts;
-        for (int num : arr) {
-            occur[num]++;
-        }
-        for (const auto& entry : occur) {
-            int count = entry.second;
-            if (counts.find(count) != counts.end()) {
-                return false;  
+        int i = 0;
+        sort(arr.begin(),arr.end());
+        vector<int> ans;
+        while (i < arr.size()){
+            int count = 1;
+            for (int j = i+1; j< arr.size(); j++){
+                if (arr[i] == arr[j])
+                    count++;
             }
-            counts.insert(count);
+            ans.push_back(count);
+            i = i + count;
+        }
+        sort(ans.begin(),ans.end());
+        for (int i = 0; i < ans.size() - 1; i++){
+            if (ans[i] == ans [i+1])
+                return false;
         }
         return true;
     }
