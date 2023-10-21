f28e16c587b9c3a32e8ea74cbbc4a53ac4b4fa39
Sadra Manouchehrifar <71413305+sadramanouch@users.noreply.github.com>
2023-10-20 23:54:28 -0700

    Time: 2 ms (47.48%), Space: 6.4 MB (48.99%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.cpp
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-        std::vector<double> ans(2, 0);
+        vector<double> ans(2, 0);
         ans[0] = celsius + 273.15;
         ans[1] = (celsius * 1.8) + 32;
         return ans;
