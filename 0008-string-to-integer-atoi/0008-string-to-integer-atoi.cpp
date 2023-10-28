e0c2ab8855689af9a63e9df7d8e7c0b5f48bad27
Sadra Manouchehrifar <71413305+sadramanouch@users.noreply.github.com>
2023-10-28 12:51:39 -0700

    Time: 0 ms (100.00%), Space: 6.6 MB (50.40%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -5,32 +5,27 @@ public:
         int n = s.length();
         int sign = 1;
         long long result = 0; 
-
         while (i < n && isspace(s[i])) {
             i++;
         }
-
         if (i < n && (s[i] == '+' || s[i] == '-')) {
-            sign = (s[i] == '-') ? -1 : 1;
+            if (s[i] == '-'){
+                sign = -1;
+            }
             i++;
         }
-
         while (i < n && isdigit(s[i])) {
             result = result * 10 + (s[i] - '0');
 
-            // Check for integer overflow
             if (result * sign > INT_MAX) {
                 return INT_MAX;
             }
             if (result * sign < INT_MIN) {
                 return INT_MIN;
             }
-
             i++;
         }
-
         result *= sign;
-
         return static_cast<int>(result);
     }
 };
