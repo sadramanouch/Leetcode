3f1cb5207083a7ed7c8ff8bf746711871e57b695
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-01-03 17:50:43 -0800

    Time: 32 ms (84.57%), Space: 9.3 MB (30.09%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -4,20 +4,17 @@ public:
         if(nums.empty()) {
             return {};
         }
-        
         sort(nums.begin(), nums.end());
-        
         int n = nums.size();
-        vector<int> dp(n, 1), parent(n, -1);
-        
-        int maxLen = 1, endIdx = 0;
-        
+        vector<int> dp(n, 1);
+        vector <int> parent(n, -1);
+        int maxLen = 1;
+        int endIdx = 0;
         for (int i = 1; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
                 if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
                     dp[i] = dp[j] + 1;
                     parent[i] = j;
-                    
                     if (dp[i] > maxLen) {
                         maxLen = dp[i];
                         endIdx = i;
@@ -25,14 +22,12 @@ public:
                 }
             }
         }
-        
-        vector<int> result;
+        vector<int> res;
         while (endIdx != -1) {
-            result.push_back(nums[endIdx]);
+            res.push_back(nums[endIdx]);
             endIdx = parent[endIdx];
         }
-        
-        reverse(result.begin(), result.end());
-        return result;
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
