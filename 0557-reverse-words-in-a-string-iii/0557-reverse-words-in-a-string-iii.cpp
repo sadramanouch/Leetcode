cfd9661115cc83e8d6828245ff93ef305cadef80
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-01-22 18:43:45 -0800

    Time: 17 ms (53.12%), Space: 12.1 MB (30.66%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -6,8 +6,8 @@ public:
     string reverseWords(string s) {
         stack<char> tmp;
         string result = "";
-        
-        for(int i = 0; i < s.size(); i++) {
+
+        for (int i = 0; i < s.size(); i++) {
             if (s[i] == ' ') {
                 result += ' ';
                 while (!tmp.empty()) {
@@ -16,18 +16,16 @@ public:
             } else {
                 while (i < s.size() && s[i] != ' ') {
                     tmp.push(s[i]);
-                    i++;  // Increment i to move to the next character
+                    i++;
                 }
                 while (!tmp.empty()) {
-                    char res = tmp.top();  // Use top() to get the top element
+                    char res = tmp.top();
                     tmp.pop();
                     result += res;
                 }
-                i--;  // Decrement i to correct the index after the loop
+                i--;
             }
         }
-        
         return result;
     }
 };
-
