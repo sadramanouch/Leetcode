ab2446b9127abe27979fbc7d84452fb6c27062f7
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-01-22 18:43:09 -0800

    Time: 19 ms (36.23%), Space: 12.1 MB (30.66%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,32 @@
+#include <stack>
+#include <string>
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<char> tmp;
+        string result = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ' ') {
+                result += ' ';
+                while (!tmp.empty()) {
+                    tmp.pop();
+                }
+            } else {
+                while (i < s.size() && s[i] != ' ') {
+                    tmp.push(s[i]);
+                    i++;
+                }
+                while (!tmp.empty()) {
+                    char res = tmp.top();
+                    tmp.pop();
+                    result += res;
+                }
+                i--;
+            }
+        }
+
+        return result;
+    }
+};
