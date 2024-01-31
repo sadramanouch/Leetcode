a0ca33f6322cbd83904d6366c20c765c80d7c20b
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-01-31 12:03:54 -0800

    Time: 3 ms (22.79%), Space: 7.2 MB (11.37%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+
+class Solution {
+public:
+    int findComplement(int num) {
+        unsigned mask = ~0;
+        while( mask & num ) mask = mask << 1;
+        return ~num ^ mask;
+    }
+};
