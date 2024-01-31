c329e6e5d93a3cc824f57b009bf3465f5b9f8436
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-01-31 12:04:13 -0800

    Time: 0 ms (100.00%), Space: 7.1 MB (11.37%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -4,7 +4,9 @@ class Solution {
 public:
     int findComplement(int num) {
         unsigned mask = ~0;
-        while( mask & num ) mask = mask << 1;
+        while( mask & num ){
+            mask = mask << 1;  
+        } 
         return ~num ^ mask;
     }
 };
