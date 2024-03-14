c541128f578aa78062a85f605923d03326aca666
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-03-14 10:28:02 -0700

    Time: 77 ms (40.19%), Space: 59.3 MB (65.55%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void deep(TreeNode* root, int level, unordered_map<int, int> &hash){
+        if (!root) {
+            return;
+        }
+        if (!root->left && !root->right){
+            hash[level] += root->val;
+            return;
+        }
+        deep(root->left, level+1, hash);
+        deep(root->right, level+1, hash);
+    }
+    
+    int deepestLeavesSum(TreeNode* root) {
+        unordered_map<int, int> hash;
+        int level = 0;
+        deep(root, level, hash);
+        int deepestLevel = 0;
+        for (const auto& pair : hash) {
+            deepestLevel = std::max(deepestLevel, pair.first);
+        }
+        
+        return hash[deepestLevel];
+    }
+};
