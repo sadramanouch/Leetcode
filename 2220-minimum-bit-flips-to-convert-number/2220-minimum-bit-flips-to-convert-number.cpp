c63770a9736335050ffcb3db558d574580e632c7
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-03-29 13:50:00 -0700

    Time: 0 ms (100.00%), Space: 7.3 MB (28.17%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,16 @@
+#include <bitset>
+
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        string startBinary = std::bitset<sizeof(int) * 8>(start).to_string();
+        string goalBinary = std::bitset<sizeof(int) * 8>(goal).to_string();
+        int count = 0;
+        int x = start ^ goal;
+        while (x) {
+            count += x & 1;
+            x >>= 1;
+        }
+        return count;
+    }
+};
