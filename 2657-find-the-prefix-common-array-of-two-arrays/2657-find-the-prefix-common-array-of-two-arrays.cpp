40d30bbd371cbddfa6ef511a5800fa9d846d426b
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-04-26 13:05:02 -0700

    Time: 113 ms (6.10%), Space: 121.8 MB (5.07%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        std::vector<int> prefixCommonArray;
-        std::set<int> seen_in_A, seen_in_B;
-
+        vector<int> prefixCommonArray;
+        set<int> seen_in_A, seen_in_B;
         for (size_t i = 0; i < A.size(); ++i) {
-            // Add the current elements to the respective sets
             seen_in_A.insert(A[i]);
             seen_in_B.insert(B[i]);
-
-            // Find the intersection of the sets
-            std::set<int> common_elements;
-            std::set_intersection(
+            set<int> common_elements;
+            set_intersection(
                 seen_in_A.begin(), seen_in_A.end(),
                 seen_in_B.begin(), seen_in_B.end(),
-                std::inserter(common_elements, common_elements.begin())
+                inserter(common_elements, common_elements.begin())
             );
-
-            // The count of common elements is the size of the intersection
             prefixCommonArray.push_back(common_elements.size());
         }
-
         return prefixCommonArray;
     }
 };
