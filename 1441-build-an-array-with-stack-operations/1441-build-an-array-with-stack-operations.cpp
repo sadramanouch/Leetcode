22e48a5e065d41e9169e32cb2cd976d150932f38
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-05-03 10:56:59 -0700

    Time: 4 ms (34.25%), Space: 9.4 MB (59.10%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        std::vector<std::string> res;
-        int targetIndex = 0;  // To track the current target position
-
-        // Iterate from 1 to n
+        vector<string> res;
+        int targetIndex = 0;  
         for (int i = 1; i <= n; i++) {
             if (targetIndex >= target.size()) {
-                break;  // If target is already built, exit early
+                break;  
             }
-
             if (target[targetIndex] == i) {
-                // If `i` is part of the target sequence, push it
                 res.push_back("Push");
-                targetIndex++;  // Move to the next target index
-            } else {
-                // If `i` is not in target, push and then pop
+                targetIndex++; 
+            } 
+            else {
                 res.push_back("Push");
                 res.push_back("Pop");
             }
         }
-
         return res;
     }
 };
