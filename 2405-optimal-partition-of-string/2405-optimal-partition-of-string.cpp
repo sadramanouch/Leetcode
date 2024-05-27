40dffe5b2ae944cc58c7bba3a2dc5fa48f85db33
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-05-26 20:24:13 -0700

    Time: 157 ms (38.46%), Space: 46.9 MB (32.96%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int partitionString(string s) {
-        unordered_set<char> charSet;
-        int Count = 1;
-        
-        for (char c : s) {
-            if (charSet.count(c)) {
-                Count++;
+        unordered_set<char> charSet; 
+        int count = 1;
+        for (auto c: s){
+            if (charSet.count(c)){
+                count++;
                 charSet.clear();
             }
             charSet.insert(c);
         }
-        
-        return Count;
+        return count;
     }
 };
