bd0bf143e60c1bcad4a841c231381dab870aca08
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-06-14 17:27:11 -0700

    Time: 0 ms (100.00%), Space: 7.5 MB (13.69%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -4,17 +4,16 @@ public:
         if (s.empty()) {
             return 0;
         }
-        
-        // Check if the entire string is a palindrome
-        int left = 0, right = s.length() - 1;
+        int left = 0;
+        int right = s.length() - 1;
         while (left < right) {
             if (s[left] != s[right]) {
-                return 2; // If not a palindrome, can be removed in two steps
+                return 2; 
             }
             left++;
             right--;
         }
         
-        return 1; // If it's a palindrome, can be removed in one step
+        return 1; 
     }
 };
