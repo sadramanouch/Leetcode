df2c86e5c6ae3f1adf36267182e75d9d2efc0873
Sadra Khorvash <71413305+sadramanouch@users.noreply.github.com>
2024-08-01 18:28:11 -0700

    Time: 3 ms (38.11%), Space: 8.5 MB (17.06%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -3,18 +3,21 @@ public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> res;
         int n = nums.size();
-        
-        if (n == 0) return res;  
+        if (n == 0){
+            return res; 
+        } 
         int start = nums[0];
-        
         for (int i = 0; i < n; i++) {
             if (i == n - 1 || nums[i + 1] != nums[i] + 1) {
                 if (start == nums[i]) {
                     res.push_back(to_string(start));
-                } else {
+                } 
+                else {
                     res.push_back(to_string(start) + "->" + to_string(nums[i]));
                 }
-                if (i != n - 1) start = nums[i + 1];
+                if (i != n - 1){
+                    start = nums[i + 1];
+                }
             }
         }
         
